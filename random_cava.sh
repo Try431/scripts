82ce470829fff552ec7eefd9a2e09bfdc42e527b
Isaac Garza <garzai@alum.mit.edu>
2018-12-05 20:24:11 -0800

    Made audio sink dynamic

--- random_cava.sh
+++ random_cava.sh
@@ -1,5 +1,7 @@
 sed -i -e 's/^gradient_color_1 = ".*"/gradient_color_1 = "#'$(openssl rand -hex 3)'"/' ~/.config/cava/config
 sed -i -e 's/^gradient_color_2 = ".*"/gradient_color_2 = "#'$(openssl rand -hex 3)'"/' ~/.config/cava/config
+
+CAVA_SOURCE_NAME=`pacmd list-sink-inputs | grep -e "sink: \<.*\>" | cut -f2 -d"<" | cut -f1 -d">"` 
 pkill -USR1 cava # This reloads the configuration file, similar to pressing 'r' (see README)
 while [ -z "$CAVA_SINK_INDEX" ]
 do
@@ -7,4 +9,7 @@ do
 	# https://stackoverflow.com/a/48070534/3614985
 	CAVA_SINK_INDEX=`pacmd list-source-outputs |  tr '\n' '\r' | perl -pe 's/ *index: ([0-9]+).+?application\.name = "([^\r]+)"\r.+?(?=index:|$)/\2:\1\r/g' | tr '\r' '\n' | grep cava | cut -f2 -d":"`
 done
-pacmd move-source-output $CAVA_SINK_INDEX alsa_output.pci-0000_00_1f.3.analog-stereo.monitor
+CAVA_SOURCE_NAME="$CAVA_SOURCE_NAME.monitor"
+# echo $CAVA_SOURCE_NAME
+# echo $CAVA_SINK_INDEX
+pacmd move-source-output $CAVA_SINK_INDEX $CAVA_SOURCE_NAME
