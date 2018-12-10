1590b91b2a8facc50dad8e6cdc0b05931f30a0c1
Isaac Garza <garzai@alum.mit.edu>
2018-12-10 11:10:41 -0800

    Removed echo statements

--- random_cava.sh
+++ random_cava.sh
@@ -2,7 +2,7 @@ sed -i -e 's/^gradient_color_1 = ".*"/gradient_color_1 = "#'$(openssl rand -hex 
 sed -i -e 's/^gradient_color_2 = ".*"/gradient_color_2 = "#'$(openssl rand -hex 3)'"/' ~/.config/cava/config
 
 CAVA_SOURCE_NAME=`pacmd list-sink-inputs | grep -e "sink: \<.*\>" | cut -f2 -d"<" | cut -f1 -d">"` 
-pkill -USR1 cava # This reloads the configuration file, similar to pressing 'r' (see README)
+pkill -USR1 cava # This reloads the configuration file, similar to pressing 'r' (see Cava README)
 while [ -z "$CAVA_SINK_INDEX" ]
 do
 	sleep 1s
@@ -10,6 +10,4 @@ do
 	CAVA_SINK_INDEX=`pacmd list-source-outputs |  tr '\n' '\r' | perl -pe 's/ *index: ([0-9]+).+?application\.name = "([^\r]+)"\r.+?(?=index:|$)/\2:\1\r/g' | tr '\r' '\n' | grep cava | cut -f2 -d":"`
 done
 CAVA_SOURCE_NAME="$CAVA_SOURCE_NAME.monitor"
-# echo $CAVA_SOURCE_NAME
-# echo $CAVA_SINK_INDEX
 pacmd move-source-output $CAVA_SINK_INDEX $CAVA_SOURCE_NAME
