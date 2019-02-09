da42211050d039d3ce39306e39280e86b2f80522
Isaac Garza <garzai@alum.mit.edu>
2019-02-09 13:23:05 -0800

    Added audio sink change functionality

--- pactl_change_sink.sh
+++ pactl_change_sink.sh
@@ -6,3 +6,9 @@ pactl list short sink-inputs | while read stream; do
 	streamId=$(echo $stream | cut '-d ' -f1)
 	pactl move-sink-input "$streamId" "$newSink"
 done
+
+# Changes $audio_sink i3 variable to new sink so I can change HDMI volume with Fn keys
+sed -i -e 's/set \$audio_sink .*/set \$audio_sink '$newSink'/' ~/.config/i3/config
+# Changes sink in polybar so polybar will show correct audio sink volume
+sed -i -e 's/sink = .*/sink = '$newSink'/' ~/.config/polybar/config
+echo $newSink
