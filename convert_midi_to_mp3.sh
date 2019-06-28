6607ece7c69bac16b198c81cae9ffa68b42faa76
Isaac Garza <ig8230@att.com>
2019-06-28 11:51:02 -0700

    Added arguments

--- convert_midi_to_mp3.sh
+++ convert_midi_to_mp3.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-for file in $HOME/Downloads/ACC_midis/*.mid; do
+source_dir=$1
+echo $source_dir
+dest_dir=$2
+for file in "$source_dir"/*.mid; do
 	FILENAME=$(echo $file | cut -d . -f 1)
 	MIDI_FILE=$FILENAME.mid
 	MP3_FILE=$FILENAME.mp3
 	fluidsynth -F $MP3_FILE /usr/share/sounds/general-user/GeneralUser_GS_v1.471.sf2 $MIDI_FILE
 done
 
-mv ~/Downloads/ACC_midis/*.mp3 ~/Downloads/ACC_mp3s/
+mkdir -p "$dest_dir" && mv "$source_dir"/*.mp3 "$dest_dir"
