daabbe60f919cb09f48a607a12b1a1fa0b5858bb
Isaac Garza <garzai@alum.mit.edu>
2019-09-24 21:49:48 -0700

    Changing which midi library to use

--- convert_midi_to_mp3.sh
+++ convert_midi_to_mp3.sh
@@ -7,7 +7,7 @@ for file in "$source_dir"/*.mid; do
 	FILENAME=$(echo $file | cut -d . -f 1)
 	MIDI_FILE=$FILENAME.mid
 	MP3_FILE=$FILENAME.mp3
-	fluidsynth -F $MP3_FILE /usr/share/sounds/general-user/GeneralUser_GS_v1.471.sf2 $MIDI_FILE
+	fluidsynth -F $MP3_FILE /usr/share/sounds/sf2/FluidR3_GM.sf2 $MIDI_FILE
 done
 
 mkdir -p "$dest_dir" && mv "$source_dir"/*.mp3 "$dest_dir"
